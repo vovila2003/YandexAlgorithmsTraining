68de85da6a8aa5a72db166f0d932b17315df988f
vovila <vovila2003@yandex.ru>
2024-03-03 22:55:25 +0300

    do task G

--- task1g.cpp
+++ task1g.cpp
@@ -1,7 +1,22 @@
 #include "task1g.h"
 #include <cmath>
+#include <memory>
+#include <iostream>
+#include <queue>
+#include <limits>
 
 using namespace std;
+
+struct Node {
+    Node* parent;
+    int x;
+    int y;
+    int soldiers;
+    int round;
+    bool done;
+};
+
+int minRound;
 
 Task1G::Task1G()
 {
@@ -12,157 +27,149 @@ void Task1G::doTask()
 {
     int x, y, p;
     cin >> x >> y >> p;
-    int round = calculateRounds(x, y, p);
+    int round = calculate(x, y, p);
     cout << round;
 }
 
 void Task1G::test()
 {
-    cout << calculateRounds(10, 11, 15) << " = 4" << "\n";
-    cout << calculateRounds(1, 2, 1) << " = -1" << "\n";
-    cout << calculateRounds(1, 1, 1) << " = 1" << "\n";
-    cout << calculateRounds(25, 200, 10) << " = 13" << "\n";
-    cout << calculateRounds(250, 500, 187) << " = 4" << "\n";
-    cout << calculateRounds(250, 500, 225) << " = 7" << "\n";
-    cout << calculateRounds(300, 301, 484) << " = 6" << "\n";
-    cout << calculateRounds(250, 500, 230) << " = 8" << "\n";
-    cout << calculateRounds(250, 500, 249) << " = 101" << "\n";
-    cout << calculateRounds(25, 100, 10) << " = 8" << "\n";
-    cout << calculateRounds(250, 500, 218) << " = 6" << "\n";
+    cout << calculate(10, 11, 15) << " = 4" << "\n";
+    cout << calculate(1, 2, 1) << " = -1" << "\n";
+    cout << calculate(1, 1, 1) << " = 1" << "\n";
+    cout << calculate(25, 200, 10) << " = 13" << "\n";
+    cout << calculate(250, 500, 187) << " = 4" << "\n";
+    cout << calculate(250, 500, 225) << " = 7" << "\n";
+    cout << calculate(300, 301, 484) << " = 6" << "\n";
+    cout << calculate(250, 500, 230) << " = 8" << "\n";
+    cout << calculate(250, 500, 249) << " = 101" << "\n";
+    cout << calculate(25, 100, 10) << " = 6" << "\n";
+    cout << calculate(250, 500, 218) << " = 6" << "\n";
+    cout << calculate(9, 427, 1) << " = 54" << "\n";
 }
 
-int Task1G::calculateRounds(int x, int y, int p)
+int Task1G::calculate(int x, int y, int p)
 {
-    int round = 1;
-    int soldiers = 0;
-    double k = (1 + sqrt(5.0)) / 2.0;
-    int prevX = x;
-    int prevY = y;
-    int prevSoldiers = 0;
-    cout << "round = 0; x = " << x
-         << "; y = " << y << "; soldiers = " << soldiers << "\n";
-    int yy, xx, ss, rr;
-    bool save = true;
-    while (x > 0) {
+    minRound = std::numeric_limits<int>::max();
+    auto rootNode = make_shared<Node>();
+    rootNode->x = x;
+    rootNode->y = y;
+    rootNode->soldiers = 0;
+    rootNode->round = 0;
+    rootNode->done = false;
 
-        double koef = (y + soldiers) * 1.0 / x;
-        if (y > x) {
-            koef = (y + soldiers + p) * 1.0 / x;
-        }
-        if ( koef <= k) {
-            if (save) {
-                yy = y;
-                xx = x;
-                ss = soldiers;
-                rr = round;
-                save = false;
-            }
-            int damage = x > y ? y : x;
-            y -= damage;
-            soldiers -= x - damage;
-            cout << "attack barrack; ";
-        } else {
-            if (3 * x >= 2 * (soldiers + y)) {
-                int damage = x > y ? y : x;
-                y -= damage;
-                soldiers -= x - damage;
-                cout << "attack barrack; ";
-            } else {
-                int damage = x > soldiers ? soldiers : x;
-                soldiers -= damage;
-                y -= x - damage;
-                cout << "attack soldiers; ";
-            }
-        }
+    process(rootNode, p);
 
-
-
-        x -= soldiers;
-        if (y > 0) {
-            soldiers += p;
-        }
-        cout.precision(4);
-        cout << "round = " << round << "; koef = "  << koef << "; x = " << x
-             << "; y = " << y << "; soldiers = " << soldiers << "\n";
-        if (y <= 0 && soldiers <= 0) {
-            break;
-        }
-
-        round++;
-        if (x == prevX && y == prevY && soldiers == prevSoldiers) {
-            round = -1;
-            break;
-        }
-        prevSoldiers = soldiers;
-        prevX = x;
-        prevY = y;
+    if (minRound == std::numeric_limits<int>::max()) {
+        return -1;
     }
-    int firstAnswer = x < 0 ? -1 : round;
-    x = xx;
-    y = yy;
-    soldiers = ss;
-    round = rr;
-    prevSoldiers = soldiers;
-    prevX = x;
-    prevY = y;
-    bool attackSoldiersFirst = true;
-    while (x > 0) {
+    return minRound;
+}
 
-        double koef = (y + soldiers) * 1.0 / x;
-        if (y > x) {
-            koef = (y + soldiers + p) * 1.0 / x;
+shared_ptr<Node> Task1G::processSoldiersAttack(Node* parent, int p)
+{
+    auto result = make_shared<Node>();
+    result->parent = parent;
+    result->done = false;
+
+    if (parent->round >= minRound) {
+        result->done = true;
+        return result;
+    }
+
+    int x = parent->x;
+    int y = parent->y;
+    int soldiers = parent->soldiers;
+    int round = parent->round;
+
+    int damage = x > soldiers ? soldiers : x;
+    soldiers -= damage;
+    y -= x - damage;
+
+    x -= soldiers;
+    if (y > 0) {
+        soldiers += p;
+    }
+    round++;
+    result->x = x;
+    result->y = y;
+    result->soldiers = soldiers;
+    result->round = round;
+
+    if (y <= 0 && soldiers <= 0) {
+        result->done = true;
+        if (minRound > round) {
+            minRound = round;
         }
-        if (attackSoldiersFirst) {
-            int damage = x > soldiers ? soldiers : x;
-            soldiers -= damage;
-            y -= x - damage;
-            cout << "attack soldiers; ";
-            attackSoldiersFirst = false;
+    }
+
+    if (x <= 0 || (x == parent->x && y == parent->y && soldiers == parent->soldiers)) {
+        result->done = true;
+    }
+    return result;
+}
+
+std::shared_ptr<Node> Task1G::processBarrackAttack(Node *parent, int p)
+{
+    auto result = make_shared<Node>();
+    result->parent = parent;
+    result->done = false;
+
+    if (parent->round >= minRound) {
+        result->done = true;
+        return result;
+    }
+
+    int x = parent->x;
+    int y = parent->y;
+    int soldiers = parent->soldiers;
+    int round = parent->round;
+
+    int damage = x > y ? y : x;
+    y -= damage;
+    soldiers -= x - damage;
+
+    x -= soldiers;
+    if (y > 0) {
+        soldiers += p;
+    }
+    round++;
+    result->x = x;
+    result->y = y;
+    result->soldiers = soldiers;
+    result->round = round;
+
+    if (y <= 0 && soldiers <= 0) {
+        result->done = true;
+        if (minRound > round) {
+            minRound = round;
         }
-        else if ( koef <= k) {
-            int damage = x > y ? y : x;
-            y -= damage;
-            soldiers -= x - damage;
-            cout << "attack barrack; ";
-        } else {
-            if (3 * x >= 2 * (soldiers + y)) {
-                int damage = x > y ? y : x;
-                y -= damage;
-                soldiers -= x - damage;
-                cout << "attack barrack; ";
-            } else {
-                int damage = x > soldiers ? soldiers : x;
-                soldiers -= damage;
-                y -= x - damage;
-                cout << "attack soldiers; ";
+    }
+
+    if (x <= 0 || (x == parent->x && y == parent->y && soldiers == parent->soldiers)) {
+        result->done = true;
+    }
+    return result;
+}
+
+void Task1G::process(shared_ptr<Node> root, int p)
+{
+    queue<shared_ptr<Node>> queue;
+    queue.push(root);
+
+    while (!queue.empty()) {
+        auto node = queue.front();
+        queue.pop();
+
+        auto attackSoldierNode = processSoldiersAttack(node.get(), p);
+        if (!attackSoldierNode->done) {
+            queue.push(std::move(attackSoldierNode));
+        }
+        double k = (node->y + p) * 1.0 / node->x;
+        if (k < 2) {
+            auto attackBarrackNode = processBarrackAttack(node.get(), p);
+            if (!attackBarrackNode->done) {
+                queue.push(std::move(attackBarrackNode));
             }
         }
-
-
-
-        x -= soldiers;
-        if (y > 0) {
-            soldiers += p;
-        }
-        cout.precision(4);
-        cout << "round = " << round << "; koef = "  << koef << "; x = " << x
-             << "; y = " << y << "; soldiers = " << soldiers << "\n";
-        if (y <= 0 && soldiers <= 0) {
-            break;
-        }
-
-        round++;
-        if (x == prevX && y == prevY && soldiers == prevSoldiers) {
-            round = -1;
-            break;
-        }
-        prevSoldiers = soldiers;
-        prevX = x;
-        prevY = y;
     }
-    int secondAnswer = x < 0 ? -1 : round;
-    if (firstAnswer == secondAnswer) return firstAnswer;
-    if (firstAnswer == -1 && secondAnswer != -1) return secondAnswer;
-    if (firstAnswer != -1 && secondAnswer == -1) return firstAnswer;
-    return min(firstAnswer, secondAnswer);
 }
--- task1g.h
+++ task1g.h
@@ -1,7 +1,9 @@
 #ifndef TASK1G_H
 #define TASK1G_H
 
-#include <iostream>
+#include <memory>
+
+struct Node ;
 
 class Task1G
 {
@@ -10,7 +12,10 @@ public:
     void doTask();
     void test();
 private:
-    int calculateRounds(int x, int y, int p);
+    int calculate(int x, int y, int p);
+    std::shared_ptr<Node> processSoldiersAttack(Node* parent, int p);
+    std::shared_ptr<Node> processBarrackAttack(Node* parent, int p);
+    void process(std::shared_ptr<Node> root, int p);
 };
 
 #endif // TASK1G_H
